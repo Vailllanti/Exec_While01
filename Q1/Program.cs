31d0a295c0944d001082306d268c9750b8ed40c3
建均 陳 <protosscarrier@gmail.com>
2022-10-21 17:54:01 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		//1+ 2 + 3 + 4 + .... + N <105
+		//請問 N 是多少?
+		static void Main(string[] args)
+		{
+			int initialize = 1;
+			int sum = 105;
+		
+			while (initialize < sum) 
+			{
+				//initialize = initialize +1
+			}
+		}
+	}
+}
